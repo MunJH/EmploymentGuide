9a0a775574d16abb81f0dd8c6857b0611d700657
MunJH <zero4125966@gmail.com>
2019-05-28 16:21:13 +0900

    구조 생성

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const morgan = require("morgan");
+
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+    console.log(app.get("port"), "번 포트에서 대기 중");
+});
