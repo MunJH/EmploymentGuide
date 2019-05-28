1e03328e4240259f7ff6d34cf6d85bc8034e37a7
MunJH <zero4125966@gmail.com>
2019-05-28 16:46:29 +0900

    구조 생성

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 
-
 const app = express();
+app.set("views", path.join(__dirname, "views"));
 app.set("port", process.env.PORT || 3000);
 
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(
     express.urlencoded({
