d249be5265eefcfb30ab693ece1ad649ea535c91
dlaqkqh1 <dlacksdn12@naver.com>
2019-05-29 21:28:11 +0900

    certification.ejs update

--- routes/page.js
+++ routes/page.js
@@ -13,4 +13,25 @@ router.get('/', (req, res, next) => {
 	});
 });
 
+router.get('/cpfield', (req, res, next) => {
+	var job = req.param('job');
+	var q1 = "SELECT certificate_NAME FROM job_has_certificate JC, CERTIFICATE C, JOB J WHERE JC.CERTIFICATE_ID = C.CERTIFICATE_id AND JC.JOB_ID = J.JOB_ID AND J.JOB_NAME = '"+job+"'";
+	var q2= "SELECT language_name FROM job_has_language JL, language L, JOB J WHERE JL.language_id = L.language_id AND JL.JOB_ID = J.JOB_ID AND J.JOB_NAME = '"+job+"'";
+	var q3 = "SELECT community_name, site FROM job_has_community JC, community C, JOB J WHERE JC.community_id = C.community_id AND JC.JOB_ID = J.JOB_ID AND J.JOB_NAME = '"+job+"'";
+	con.query(q1, function (err, result, fields) {
+		certifi = result;
+		con.query(q2, function (err, result, fields) {
+			lang = result;
+			con.query(q3, function (err, result, fields) {
+				com = result;
+				res.render('certification.ejs', {
+					certification : certifi,
+					language : lang,
+					community : com
+				});
+			});
+		});
+	});
+});
+
 module.exports = router;
